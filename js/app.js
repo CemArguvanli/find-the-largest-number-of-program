b9e6828a3eb8e97431934981e84c0e25cf083e99
Cem <cemarguvanli@gmail.com>
2014-12-18 17:03:03 +0200

    find big number

--- js/app.js
+++ js/app.js
@@ -3,17 +3,14 @@ for (var i = 0; i < 10; i++) {
     sayilar[i] = prompt(i + ".nci sayıyı girin");
     document.write(sayilar[i] + "<br/>");
 }
-var enb, enk;
+var enb;
 enb = parseInt(sayilar[0]);
-enk = parseInt(sayilar[0]);
+
 for (var i = 0; i < 10; i++) {
     if (parseInt(sayilar[i]) > enb) {
         enb = parseInt(sayilar[i]);
     }
 }
-for (var i = 0; i < 10; i++) {
-    if (parseInt(sayilar[i]) < enk) {
-        enk = parseInt(sayilar[i]);
-    }
-}
-document.write("en büyük sayı  =" + enb + " en küçük sayı= " + enk);
+
+var container = document.getElementById("container");
+container.innerHTML = "<span>en büyük sayı   " + enb + "  </span>"
